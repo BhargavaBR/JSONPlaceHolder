5149e522e64c1c394830f7a65028e1d93a21233c
BhargavaBR <bhargavravi143@gmail.com>
2023-04-29 16:53:01 +0530

    Added okHttp Client into Project
    Signed-off-by: BhargavaBR <bhargavravi143@gmail.com>

--- JSONPlaceHolder/src/main/java/org/example/Main.java
+++ JSONPlaceHolder/src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import org.example.http.Client;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        Client client = new Client();
+        String response = client.get("http://example.com");
+        System.out.println(response);
     }
 }
